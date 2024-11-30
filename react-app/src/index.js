8245b35cba73b3029c80a32447810f233125fcda
mahdi <127404389+mahdiahmadi80@users.noreply.github.com>
2024-11-30 19:03:32 +0330

    start project
    add react

---
+++ react-app/src/index.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import ReactDom from 'react-dom'
+
+const element = (<div>hello react</div>)
+const root = document.getElementById('root')
+ReactDom.render(element, root)
